561c82ea391c030040e2a875064647c350ca644b
Sean Yap <syap0317@gmail.com>
2021-03-02 22:28:42 -0500

    add countries React state and create dropdown ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 import './App.css';
 
 function App() {
+  // React useState hook
+  const [countries, setCountries] = useState(['USA', 'Malaysia', 'UK']);
+
+  // React useEffect hook which calls itself automatically when this function is called OR depending on arguments passed
+  // useEffect(() => {
+
+  // })
+
   return (
     <div className="app">
       {/* Components */}
       {/* Title and Select input dropdown */}
       <h1>Covid-19-Tracker</h1>
-
+      <FormControl className="app__dropdown">
+        <Select>
+          {/* 1. loop through all the countries that was pulled in from api service in useEffect hook */}
+          {/* 2. return a MenuItem component for each country */}
+          {/* JS code in HTML has to be in curly braces */}
+          {countries.map(country => (
+            <MenuItem value={country}>{country}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       {/* 3 Infoboxes */}
 
       {/* Table */}
