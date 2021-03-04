50d81f2081c3dbce36f27cff8726a9aa5463a208
Sean Yap <syap0317@gmail.com>
2021-03-03 19:32:24 -0500

    make and process API call to display on dropdown

--- src/App.js
+++ src/App.js
@@ -1,31 +1,67 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FormControl, Select, MenuItem } from "@material-ui/core";
 import './App.css';
 
 function App() {
   // React useState hook
-  const [countries, setCountries] = useState(['USA', 'Malaysia', 'UK']);
+  // countries is an array that stores a list of objects which contain mappings of name -> country AND value -> country-iso2
+  const [countries, setCountries] = useState([]);
+  // keep track of current selected country from dropdown list; default worldwide
+  const [country, setCountry] = useState("worldwide");
 
-  // React useEffect hook which calls itself automatically when this function is called OR depending on arguments passed
-  // useEffect(() => {
+  // React useEffect hook 
+  // code inside anonymous func will run only once when the component loads AND if there's additional state in hook arg
+  useEffect(() => {
+    // async -> send a request, wait for it in a non-blocking way, then do something with the info
+    const getCountriesData = async () => {
+      // pull data(request object) from api service end point 
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then(response => response.json())  // convert res obj to json type
+        .then(data => {
+          const countries = data.map(country => ({
+            name: country.country, // United States
+            value: country.countryInfo.iso2 // US
+          }));
+          
+          // call function to change state instead of using assignment operator
+          setCountries(countries);
+        })
+    }
 
-  // })
+    getCountriesData();
+  }, []);
+
+  // this func is called by the event listener when the click event for the select element is triggered
+  const onCountryChange = (event) => {
+    // get the value of the element that triggered the event
+    const countryCode = event.target.value;
+
+    setCountry(countryCode);
+  };
 
   return (
     <div className="app">
       {/* Components */}
+
       {/* Title and Select input dropdown */}
-      <h1>Covid-19-Tracker</h1>
-      <FormControl className="app__dropdown">
-        <Select>
-          {/* 1. loop through all the countries that was pulled in from api service in useEffect hook */}
-          {/* 2. return a MenuItem component for each country */}
-          {/* JS code in HTML has to be in curly braces */}
-          {countries.map(country => (
-            <MenuItem value={country}>{country}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      <div className="app__header">
+        <h1>Covid-19-Tracker</h1>
+        <FormControl className="app__dropdown">
+          {/* variant property is the look for the select box; value property is the value that the box display. we want the box to display the current selected country */}
+          {/* onChange property is the event listener. we need to add event listener to this select element to keep track of selected country */}
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+
+            {/* 1. loop through all the countries that was pulled in from api service in useEffect hook */}
+            {/* 2. return a MenuItem component for each country */}
+            {/* JS code in HTML has to be in curly braces */}
+            {countries.map(country => (
+              // value property is to assign a name to identify this particular element, when when it is selected you know which one
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
       {/* 3 Infoboxes */}
 
       {/* Table */}
