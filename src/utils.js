d14b9db1ca416ede1944799e0b73c9fcfd73c744
Sean Yap <syap0317@gmail.com>
2021-04-19 20:44:14 -0400

    add helper func: prettyPrintStat and showDataOnMap

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,21 @@
+import React from "react"; // to use JSX
+import numeral from "numeral"; // to format numbers
+import { Circle, Popup } from "react-leaflet";
+
 // this file contains helper functions
+
+const casesTypeColors = {
+  // this is a constant mappings of casesType: cases, recovered, and deaths to their individual assigned color
+  cases: {
+    hex: "#CC1034",
+  },
+  recovered: {
+    hex: "#7DD71D",
+  },
+  deaths: {
+    hex: "#FB4443",
+  },
+};
 
 // function expression
 export const sortData = (data) => {
@@ -14,3 +31,39 @@ export const sortData = (data) => {
   });
   return sortedData;
 };
+
+// format the stats numbers to look good when displayed
+export const prettyPrintStat = (stat) =>
+  stat ? `+${numeral(stat).format("0.0a")}` : "+0";
+
+// draw circles on the map with interactive tooltip
+export const showDataOnMap = (data, casesType = "cases") =>
+  // iterate through all countries in data
+  data.map((country) => (
+    <Circle
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      fillOpacity={0.4}
+      color={casesTypeColors[casesType].hex}
+      fillColor={casesTypeColors[casesType].hex}
+      radius={(country.cases / country.todayCases) * 300}
+    >
+      <Popup>
+        <div className="info-container">
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          />
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format("0,0")}
+          </div>
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format("0,0")}
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}
+          </div>
+        </div>
+      </Popup>
+    </Circle>
+  ));
