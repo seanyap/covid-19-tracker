bba3298df74566a2c0d123311601f7e4a16c102b
Sean Yap <syap0317@gmail.com>
2021-04-20 00:44:28 -0400

    add flag icon next to country name in table

--- src/Table.js
+++ src/Table.js
@@ -8,8 +8,19 @@ function Table({ countries }) {
     <div className="table">
       {/* for every country in countries, call anonynous function and do some action */}
       {/* destructure country and get keys: country and cases */}
-      {countries.map(({ country, cases }) => (
+      {countries.map(({ country, cases, countryInfo }) => (
         <tr>
+          <td
+            style={{
+              backgroundImage: `url(${countryInfo.flag})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              borderRadius: "50%",
+              height: "30px",
+              width: "30px",
+              marginBottom: "10px",
+            }}
+          ></td>
           <td>{country}</td>
           <td>
             <strong>{numeral(cases).format("0,0")}</strong>
