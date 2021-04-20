44d5958e755f76d0105c302b8120da1332602835
Sean Yap <syap0317@gmail.com>
2021-04-20 00:42:45 -0400

    add click event listener to infobox component

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -5,11 +5,11 @@ import "./InfoBox.css";
 // after you write the core of a component, make sure to test it first by rending it in your page.
 // if you dont have your data or a specific feature yet, come up with your own data to test it out. DO NOT GO AND CODE THE REQUIRED FEATURE BEFORE TESTING
 // takes in props which we destructured into 3 variables
-function InfoBox({ title, cases, total }) {
+function InfoBox({ title, cases, total, ...props }) {
   return (
     // make sure to assign each element a CSS BEM class names to style
     // always keep in mind the UI of the app. use library like material-UI for pre-written styling to ensure you don't spend too much time tinkering with design
-    <Card className="infoBox">
+    <Card onClick={props.onClick} className="infoBox">
       <CardContent>
         {/* Title */}
         <Typography className="infoBox__title" color="textSecondary">
